5081f50f6ed48a20b8c06aaeefb3d92e3239a8c6
Aublic-sudo <rstock937@gmail.com>
2025-06-03 15:36:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) ACE 
-
+from dotenv import load_dotenv
 import os
 
-class Config(object):
-    # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7091128043:AAF5q40JTQmn-0l1pOOf0cVCZhiLC3LEi4g")
-    API_ID = int(os.environ.get("API_ID", "25352517"))
-    API_HASH = os.environ.get("API_HASH", "2b7e6cf7752c3af91f958d67793a3e0b")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "7360968885")
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+AUTH_USERS = os.getenv("AUTH_USERS")
