2a51c013e90b82e98e2fe647727f06b8b0b051de
Aublic-sudo <rstock937@gmail.com>
2025-06-03 18:37:47 +0530

    Create masterappx.py

---
+++ plugins/masterappx.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.universal import account_login
+
+@Client.on_message(filters.command("MasterAppx") & filters.private)
+async def master_appx(bot, message: Message):
+    buttons = [
+        [InlineKeyboardButton("🔍 Find Appx URL", callback_data="find_appx")],
+    ]
+    await message.reply_text(
+        "**Send Appx API URL:**\n\n"
+        "If you don't know the Appx API URL, use the 'Find Appx URL' button below.",
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+    input1 = await bot.listen(message.chat.id)
+    api_url = input1.text.strip()
+    await input1.delete()
+    await account_login(bot, message, api_url)
