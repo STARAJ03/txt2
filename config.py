5abdd09194ed847e19dac1d97a27ccf7b8750064
Aublic-sudo <rstock937@gmail.com>
2025-06-03 14:13:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "7360968885")
