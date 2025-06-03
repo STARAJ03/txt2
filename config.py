657c5e24491cc07f3dae74c856e71917108f5c77
Aublic-sudo <rstock937@gmail.com>
2025-06-03 18:49:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 
 class Config(object):
-    # get a token from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "")
+
+    # Safe conversion of API_ID
+    API_ID = int(os.environ.get("API_ID", ""))  # default 0 agar set na ho
+
+    # AUTH_USERS ko list me convert karo agar string ho comma separated
+    AUTH_USERS = [int(x) for x in os.environ.get("AUTH_USERS", "").split(",") if x]
