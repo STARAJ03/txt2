c66af070da8829535e99804948ac93ada3eaf248
Aublic-sudo <rstock937@gmail.com>
2025-06-07 11:34:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()  # .env file load karne ke liye
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+class Config:
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "")
